43f25728c98b953d6babe6df5a838a473149df7f
shawnhr <shawnk0818@gmail.com>
2017-11-07 20:04:33 -0800

    working on sessions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,16 +2,27 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  console.log(req.cookies);
-  if (req.cookies === undefined) {
-    return models.Session.create()
+  if (Object.keys(req.cookies).length === 0) {
+    return models.Sessions.create()
       .then(result => {
-        
+        return models.Sessions.get({id: result.inserId});
       })
+      .tap(session =>{
+        // if(!session){
+        //   throw session;
+        // }
+        res.cookie('shortlyid', session.hash);
+      })
+      .then(session =>{
+        req.session = session;
+        next();
+      });
+    // .catch(() => {
+    //   return req.session;
+    // });
   }
-  next();
-
 };
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -12,50 +12,9 @@ const parseCookies = (req, res, next) => {
     req.cookies = {};
   }
   next();
-  // }
-  // for (var i = 0; i< cookieKeyValue.length; i++) {
-  //   if(i%2===0){
-  //     req.cookies[cookieKeyValue[i]] = cookieKeyValue[i+1];
-  //   }else{
-  //     let cookie = cookieKeyValue[i+1].pop();
-  //     req.cookies[cookieKeyValue[i]] = cookie;
-  //   }
-  // }
 };
     
-    
-    
-    // if (req.headers) {
-    //   console.log('headers', req.headers)
-    //   var cookieKeyValue = req.headers.cookie.split('=');
-    // }
-    // if (cookieKeyValue.length === 2) {
-    //   console.log('type:',typeof cookieKeyValue);
-    //   console.log(cookieKeyValue[0]);
-    //   req.cookies = {cookieKeyValue0 : cookieKeyValue[1]};
-    // }
-
-    // if (cookieKeyValue.length > ) {
-    //   console.log('more than one cookie', )
-    // }
-    // if (cookieKeyValue.length === 2) {
-    //   var cookieKeyValue = req.headers.cookie.split('=');
-    //   req.cookies = {cookieKeyValue[0] : cookieKeyValue[1]}
-    // } else {
-    //   var 
-    //   cookieKeyValue.forEach(function(cookie){
-        
-    //   })
-
-  // var rc = req.headers.cookie;
-  // if (req.headers.cookie) {
-  //   req.headers.cookie.split(';').forEach(function(cookie) {
-  //     var parts = cookie.split('=');
-  //     console.log('parts',parts);
-  //   });
-  // }
-    // console.log('cookie', cookieKey);
-    // return req.cookies;
+ 
 module.exports = parseCookies;
 
 
